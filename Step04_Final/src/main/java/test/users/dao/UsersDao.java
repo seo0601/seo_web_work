e3fa48d2e2a940e130d992f459b2e2e4f8832be2
Seo dong min <sdm067@nave.com>
2022-11-21 12:42:15 +0900

    Step04_Final 프로젝트 추가함

---
+++ Step04_Final/src/main/java/test/users/dao/UsersDao.java
@@ -0,0 +1,105 @@
+package test.users.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import test.users.dto.UsersDto;
+import test.util.DbcpBean;
+
+public class UsersDao {
+	
+	private static UsersDao dao;
+	//외부에서 객체를 생성할 수 없도록
+	private UsersDao() {}
+	//참조 값을 리턴해주는 static 메소드
+	public static UsersDao getInstance() {
+		if(dao == null) {
+			dao = new UsersDao();
+		}
+		return dao;
+	}
+	
+	//인자로 전달되는 dto에 있는 아이디와 비밀번호를 이용해서 해당 정보가 유효한 정보인지 여부를 리턴하는 메소드
+	public boolean isValid(UsersDto dto) {
+		
+		//아이디와 비밀번호 유효성 여부를 담을 변수를 만들고 초기값 false를 부여하기
+		boolean isValid = false;
+		
+		//필요한 객체를 담을 지역 변수를 미리 만들어 둔다.
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			//Connection pool에서 Connection 객체를 하나 얻어 온다.
+			conn = new DbcpBean().getConn();
+			//실행할 sql 문의 뼈대 구성하기
+			String sql = "SELECT id"
+					+ " FROM users"
+					+ " WHERE id=? AND pwd=?";
+			
+			pstmt = conn.prepareStatement(sql);
+			//sql 문의 ?에 바인딩 할 것이 있으면 한다.
+			pstmt.setString(1, dto.getId());
+			pstmt.setString(2, dto.getPwd());
+			//SELECT 문을 수행하고 결과 값을 받아온다.
+			rs = pstmt.executeQuery();
+			//SELECT 된 ROW가 있는지 확인해 본다.
+			if (rs.next()) {
+				isValid =true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return isValid;
+	}
+	
+	//회원 한 명의 정보를 DB에 저장하고 성공 여부를 리턴하는 메소드
+	public boolean insert(UsersDto dto) {//UsersDto에 id, pwd, email이 있다.
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int rowCount = 0;
+
+		try {
+			conn = new DbcpBean().getConn();
+			//수정할 미완성의 sql문
+			String sql = "INSERT INTO users"
+					+ " (id, pwd, email, regdate)"
+					+ " VALUES(?, ?, ?, SYSDATE)";
+			pstmt = conn.prepareStatement(sql);
+			//?에 바인딩 할 것이 있으면 해주고
+			pstmt.setString(1, dto.getId());
+			pstmt.setString(2, dto.getPwd());
+			pstmt.setString(3, dto.getEmail());
+			//INSERT OR UPDATE OR DELETE 문을 수행하고 수정,삭제,추가된 ROW의 개수를 리턴 받기
+			rowCount = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (rowCount > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+}
---
+++ Step04_Final/src/main/webapp/users/login.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%
+	//1. 폼 전송되는 아이디, 비밀번호 읽어오기
+	
+	//2. DB에 실제로 존재하는 정보인지 확인하기(맞는 정보이면 로그인 처리도 한다.)
+	
+	//3. 응답
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>/users/login.jsp</title>
+</head>
+<body>
+
+</body>
+</html>
